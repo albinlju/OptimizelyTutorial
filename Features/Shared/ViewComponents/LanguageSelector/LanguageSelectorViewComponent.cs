3f0578ca39091945e95cb9f97e389ecf27fbda20
Albin Ljung <albin.ljung@consid.se>
2024-08-27 11:45:40 +0200

    Language selector

---
+++ Features/Shared/ViewComponents/LanguageSelector/LanguageSelectorViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Globalization;
+using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Mvc;
+using OptimizelyTutorial.Features.Shared.ViewModels;
+
+namespace OptimizelyTutorial.Features.Shared.ViewComponents.LanguageSelector
+{
+    public class LanguageSelectorViewComponent(
+        ILanguageBranchRepository _languageBranchRepository,
+        IPageRouteHelper _pageRouteHelper) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() => View(new LanguageSelectorViewModel {
+          Languages =  _languageBranchRepository.ListEnabled(),
+          PreferredLanguage = ContentLanguage.PreferredCulture.Name,
+          CurrentPageLink = _pageRouteHelper.PageLink
+        });
+    }
+}
